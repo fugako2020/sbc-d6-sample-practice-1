352829443cc79c7d8607b29666e0129292fa81cb
Jed <basasjed@gmail.com>
2024-06-24 19:58:51 -0700

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,62 @@
+""""
+n = 0
+
+# Start the while loop
+while n <= 5:
+    print(n)
+   
+    n += 1
+
+
+li = [1, 2, 3, 4]
+
+
+for item in li:
+    print(item)
+
+
+string = "hi world"
+
+
+for char in string:
+    print(1)
+
+n1_ = ["jed", "basas", "flores"] 
+
+for name in n1_:
+    print(name)
+
+
+for n in range(1, 10, 2):  # 1, 3, 5, 7, 9
+    print(n)
+
+row = int(input("row: "))
+
+for i in range(row):
+    print("*")
+  
+
+#while loop
+n = 0
+while n < 10:
+    n = int(input("are you single?:D : "))
+
+    while n < 10:
+        print("single")
+
+else:
+    print("are you single :p")
+    """
+log = True
+
+while log:
+    username = input("username: ")
+    password = input("password: ")
+    if username == "single" and password == "japan":
+        print("succesfully login:")
+    else:
+        print("login failed try again")  
+
+
+
+
